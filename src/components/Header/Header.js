dc1fa874a8b7dec2f55aa4f9a56d11fc183588a9
ArafatMunna <arafatmunna000@gmail.com>
2022-03-25 22:02:33 +0600

    Header component added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="my-4 text-center">
+            <h1 className="text-primary">Laptop World</h1>
+            <h3 className="text-info">Choose your 4 favourite laptop</h3>
+        </div>
+    );
+};
+
+export default Header;
